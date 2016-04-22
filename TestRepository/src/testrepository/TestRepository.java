d668cd179796f208fbb4ac815056427b69330801
Alexander <alexander_topilskii@epam.com>
2016-04-22 15:38:40 +0300

    Test commit for 01

--- TestRepository/src/testrepository/TestRepository.java
+++ TestRepository/src/testrepository/TestRepository.java
@@ -17,6 +17,6 @@ public class TestRepository {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world!");
+        System.out.println("Hello 2");
     }
-    
 }
