278cbd1c4f882c05ba98b7da76b4e5954372446f
Alexander <alexander_topilskii@epam.com>
2016-04-22 15:54:05 +0300

    this is message commit test 02

--- TestRepository/src/testrepository/TestRepository.java
+++ TestRepository/src/testrepository/TestRepository.java
@@ -17,6 +17,7 @@ public class TestRepository {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world!"); //this my comment 
+        //This test -02
     }
     
 }
